26ab56e0e627438de7e585783bf203cd450ccbd9
Max Lamprecht <max.lamprecht@mail.schwarz>
2023-10-23 15:58:44 +0200

    fix: subnet X networkrbacs join
    Combinatorial blow-up with the SQLAlchemy strategy lazy='joined'
    betweeen subnets and networkrbacs:
    https://bugs.launchpad.net/neutron/+bug/1649317
    
    New LP Bug:
    https://bugs.launchpad.net/neutron/+bug/2037107
    
    Change-Id: I2f4f3ddc52702af7768a3907dad8ff1e744a39f1

--- neutron/db/models_v2.py
+++ neutron/db/models_v2.py
@@ -248,7 +248,7 @@ class Subnet(standard_attr.HasStandardAttributes, model_base.BASEV2,
     # subnets don't have their own rbac_entries, they just inherit from
     # the network rbac entries
     rbac_entries = orm.relationship(
-        rbac_db_models.NetworkRBAC, lazy='joined', uselist=True,
+        rbac_db_models.NetworkRBAC, lazy='subquery', uselist=True,
         foreign_keys='Subnet.network_id',
         primaryjoin='Subnet.network_id==NetworkRBAC.object_id',
         viewonly=True)
